388b8112dc418013a17bc367bf47291da578851f
TerminiUsMag <nikolaidan1@abv.bg>
2023-06-19 15:18:24 +0300

    Add project files.

---
+++ SchoolSocialMediaApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolSocialMediaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
